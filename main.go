5d4b53a8e2712727d5f9bcba76af423c5052e2ae
Earl <linux107@gmail.com>
2023-08-11 11:57:28 +0800

    update api v1

--- main.go
+++ main.go
@@ -3,25 +3,25 @@ package main
 import (
 	"fmt"
 	"gin_example/pkg/setting"
-	"github.com/gin-gonic/gin"
+	"gin_example/routers"
 	"net/http"
 )
 
 func main() {
 
-	router := gin.Default()
-	router.GET("/test", func(context *gin.Context) {
-		context.JSON(200, gin.H{
-			"message": "test",
-		})
-	})
+	//router := gin.Default()
+	//router.GET("/test", func(context *gin.Context) {
+	//	context.JSON(200, gin.H{
+	//		"message": "test",
+	//	})
+	//})
 
 	// Group Routers
-	//routerInit := routers.InitRouter()
+	routerInit := routers.InitRouter()
 
 	s := &http.Server{
 		Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
-		Handler:        router,
+		Handler:        routerInit,
 		ReadTimeout:    setting.ReadTimeout,
 		WriteTimeout:   setting.WriteTimeout,
 		MaxHeaderBytes: 1 << 20,
--- models/tag.go
+++ models/tag.go
@@ -1,10 +1,34 @@
 package models
 
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"time"
+)
+
 type Tag struct {
 	Name       string `json:"name"`
 	CreatedBy  string `json:"created_by"`
 	ModifiedBy string `json:"modified_by"`
 	State      int    `json:"state"`
+}
+
+func AddTag(name string, state int, createdBy string) bool {
+	db.Create(&Tag{
+		Name:      name,
+		State:     state,
+		CreatedBy: createdBy,
+	})
+	return true
+}
+func DeleteTag(id int) bool {
+	db.Where("id = ?", id).Delete(&Tag{})
+	return true
+}
+
+func EditTag(id int, data interface{}) bool {
+	db.Model(&Tag{}).Where("id =?", id).Update(data)
+	return true
 }
 
 func GetTags(pageNum int, pageSize int, maps interface{}) (tags []Tag) {
@@ -17,7 +41,7 @@ func GetTagTotal(maps interface{}) (count int) {
 	return
 }
 
-func ExisTagByName(name string) bool {
+func ExistTagByName(name string) bool {
 	var tag Tag
 	db.Select("id").Where("name=?", name).First(&tag)
 	if tag.ID > 0 {
@@ -26,11 +50,21 @@ func ExisTagByName(name string) bool {
 	return false
 }
 
-func Addtag(name string, state int, createdBy string) bool {
-	db.Create(&Tag{
-		Name:      name,
-		State:     state,
-		CreatedBy: createdBy,
-	})
-	return true
+func ExistTagByID(id int) bool {
+	var tag Tag
+	db.Select("id").Where("id=?", id).First(&tag)
+	if tag.ID > 0 {
+		return true
+	}
+	return false
+}
+
+func (tag *Tag) BeforeCreate(scope *gorm.Scope) error {
+	scope.SetColumn("CreatedOn", time.Now().Unix())
+	return nil
+}
+
+func (tag *Tag) BeforeUpdate(scope *gorm.Scope) error {
+	scope.SetColumn("ModifiedOn", time.Now().Unix())
+	return nil
 }
